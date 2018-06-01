67f45c7c66c1467005189282a6704ca16b1830e8
Maciej Rzepkowski <m.rzepkowski@bossa.pl>
2018-06-01 18:50:36 +0200

    Aktualziacja -5

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,6 +12,11 @@ public class Greeter {
 
   }
 
+  /**
+   * @param sdasds
+   * @return ......
+   */
+
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
